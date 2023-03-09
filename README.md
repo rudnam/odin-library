9c8dbc76c669680b9300235b83b941358f7b0434
rudnam <francisalbarracin2@gmail.com>
2023-03-09 14:44:50 +0800

    Linting

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # odin-library
+
 #### Link: https://rudnam.github.io/odin-library/
+
 ## Description
+
 Project from [TOP](https://www.theodinproject.com/lessons/node-path-javascript-library)
+
 - Objects and Object constructors practice
--- scripts/script.js
+++ scripts/script.js
@@ -1,88 +1,86 @@
+/* eslint-disable no-use-before-define */
 // script.js
 const container = document.querySelector('.container');
 const newBook = document.querySelector('#new-book');
 const addBook = document.querySelector('#add-book');
 const form = document.querySelector('#my-form');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const read = document.querySelector('#read');
+const titleElem = document.querySelector('#title');
+const authorElem = document.querySelector('#author');
+const pagesElem = document.querySelector('#pages');
+const readElem = document.querySelector('#read');
 const warn = document.querySelector('.warn');
-let myLibrary = [];
+const myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read === 'true' ? 'read' : 'not read yet';
-        this.info = function () {
-            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-        };
-    }
-}
-
-function addBookToLibrary() {
-    if (title.value && author.value && pages.value) {
-        const toAdd = new Book(title.value, author.value, pages.value, read.value);
-        myLibrary.push(toAdd);
-        displayBooks();
-        warn.style.display = 'none';
-        form.reset();
-    } else {
-        warn.style.display = 'inline';
-    }
-    
-}
-
-function displayBooks() {
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild);
-    }
-    myLibrary.forEach(function (item, index) {
-        var bookCard = document.createElement('div');
-        bookCard.classList.add('card');
-        var p1 = document.createElement('p');
-        var p2 = document.createElement('p');
-        var p3 = document.createElement('p');
-        var p4 = document.createElement('p');
-        var toggle = document.createElement('button');
-        var remove = document.createElement('button');
-        p1.innerText = `Title: ${item.title}`;
-        p2.innerText = `Author: ${item.author}`;
-        p3.innerText = `Pages: ${item.pages}`;
-        p4.innerText = `Status: ${item.read}`;
-        toggle.innerText = "TOGGLE";
-        toggle.addEventListener('click', toggleBook);
-        toggle.dataset.index = index;
-        remove.innerText = "REMOVE";
-        remove.dataset.index = index;
-        remove.addEventListener('click', removeBook);
-        bookCard.append(p1,p2,p3,p4,toggle,remove);
-        container.appendChild(bookCard);
-    });
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read === 'true' ? 'read' : 'not read yet';
+    this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  }
 }
 
 function toggleForm() {
-    if (form.style.display == 'none') {
-        form.style.display = 'flex';
-    } else {
-        form.style.display = 'none';
-    }
+  if (form.style.display === 'none') {
+    form.style.display = 'flex';
+  } else {
+    form.style.display = 'none';
+  }
 }
 
 function removeBook(e) {
-    myLibrary.splice(e.currentTarget.dataset.index, 1);
-    displayBooks();
+  myLibrary.splice(e.currentTarget.dataset.index, 1);
+  displayBooks();
 }
 
 function toggleBook(e) {
-    if (myLibrary[e.currentTarget.dataset.index].read === 'read') {
-        myLibrary[e.currentTarget.dataset.index].read = 'not yet read';
-    } else {
-        myLibrary[e.currentTarget.dataset.index].read = 'read';
-    }
+  if (myLibrary[e.currentTarget.dataset.index].read === 'read') {
+    myLibrary[e.currentTarget.dataset.index].read = 'not yet read';
+  } else {
+    myLibrary[e.currentTarget.dataset.index].read = 'read';
+  }
+  displayBooks();
+}
+
+function displayBooks() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+  myLibrary.forEach((item, index) => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
+    const p4 = document.createElement('p');
+    const toggle = document.createElement('button');
+    const remove = document.createElement('button');
+    p1.innerText = `Title: ${item.title}`;
+    p2.innerText = `Author: ${item.author}`;
+    p3.innerText = `Pages: ${item.pages}`;
+    p4.innerText = `Status: ${item.read}`;
+    toggle.innerText = 'TOGGLE';
+    toggle.addEventListener('click', toggleBook);
+    toggle.dataset.index = index;
+    remove.innerText = 'REMOVE';
+    remove.dataset.index = index;
+    remove.addEventListener('click', removeBook);
+    bookCard.append(p1, p2, p3, p4, toggle, remove);
+    container.appendChild(bookCard);
+  });
+}
+
+function addBookToLibrary() {
+  if (titleElem.value && authorElem.value && pagesElem.value) {
+    const toAdd = new Book(titleElem.value, authorElem.value, pagesElem.value, readElem.value);
+    myLibrary.push(toAdd);
     displayBooks();
+    warn.style.display = 'none';
+    form.reset();
+  } else {
+    warn.style.display = 'inline';
+  }
 }
 
 // test
